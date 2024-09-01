e1fd247ab353d2c96247f7a61758008122713f94
Ernesto Rodriguez Reina <erreina@gmail.com>
2024-09-01 15:45:17 -0400

    Setting wezterm background opacity to 1

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,13 +9,13 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Macchiato"
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 12
 
 -- config.enable_tab_bar = false
 
 -- config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 1.0
 config.macos_window_background_blur = 10
 
 -- padding
