d4366ed51bab9f031c6aa622e877cf5d7ff23272
Ernesto Rodriguez Reina <erreina@gmail.com>
2024-09-01 14:39:48 -0400

    Initial push

---
+++ nvim/lua/config/plugins/lsp/mason.lua
@@ -0,0 +1,55 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+				"bashls",
+				"rust_analyzer",
+				"yamlls",
+				"cmake",
+				"clangd",
+				"pyright",
+				"jedi_language_server",
+				-- "gopls",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"clang-format", -- c, c++ formatter
+				"rustfmt", -- rust formatter
+				"pylint", -- python linter
+				"cpplint", -- c, c++ linter
+			},
+		})
+	end,
+}
